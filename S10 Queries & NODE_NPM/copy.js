c50057f4a6f2ac983f8fa29208d490eb9283ccef
Omsai <omsainagvekar100@gmail.com>
2023-06-15 19:49:00 +0530

    Load

--- S10 Queries & NODE_NPM/copy.js
+++ S10 Queries & NODE_NPM/copy.js
@@ -1,38 +1,84 @@
-const http = require("http")
+const http = require('http')
 
 const Users = [
     {
-        name: "Bruce",
-        age: 25
-    },
-    {
-        name: "Tony",
+        name: "John",
         age: 30
     },
     {
-        name: "Steve",
-        age: 70
+        name: "Judicator",
+        age: 35
+    },
+    {
+        name: "Table",
+        age: 300
     }
 ]
 
 const server = http.createServer(function (request, response) {
 
-    const paths = request.url.split("/")
+    const paths = request.url.split('/')
 
-    console.log("paths--- ", paths)
+    console.log("---methods---", request.method)
 
-    if(paths[1] === "users" && paths.length === 2) {
+    if(request.method === "GET" && paths[1] === "users" && paths.length === 2) {
         response.write(JSON.stringify(Users))
     } 
-    else if(paths[1] === "users" && paths[2]) {
+    else if(request.method === "GET" && paths[1] === "users" && paths[2]) {
         const idx = paths[2]
         const user = Users[idx]
-        if(user) {
+        if(user){
             response.write(JSON.stringify(user))
         } else {
-            response.write("Not Found")
+            response.write("User Not Found")
         }
     }
+
+    else if (request.method === "POST" && paths[1] === "users") {
+        let data = ""
+
+        request.on("data", function (chunks) {
+            data += chunks
+        })
+
+        request.on("end", function () {
+            const obj = JSON.parse(data.toString())
+            Users.push(obj)
+        
+        })
+
+        response.statusCode = 201
+        response.write("User Created")
+    }
+
+    else if(request.method === "PUT" && paths[1] === "users" && paths[2]){
+        const idx = paths[2]
+        let data = ""
+        request.on("data", function (chunks) {
+            data += chunks
+        })
+
+        request.on("end", function () {
+            const obj = JSON.parse(data.toString())
+            Users[idx] = obj
+        })
+        response.statusCode = 200
+        response.write("User data updated.")
+        response.end()
+    }
+
+    else if(request.method === "DELETE" && paths[1] === "users" && paths[2]){
+        const idx = paths[2]
+        const user = Users[idx]
+        if(user){
+            Users.splice(idx, 1)
+            
+        }
+        response.statusCode = 200
+        response.write("User data Deleted")
+    }
+        
+
     else {
         response.write("Not Found")
     }
@@ -40,6 +86,6 @@ const server = http.createServer(function (request, response) {
     response.end()
 })
 
-server.listen(3000, function () {
-    console.log("server is running on port number 3000")
+server.listen(3000, function(){
+    console.log('Server is running on port 3000')
 })
--- S10 Queries & NODE_NPM/index.js
+++ S10 Queries & NODE_NPM/index.js
@@ -21,7 +21,7 @@ const server = http.createServer(function (request, response) {
 
     console.log("---methods---", request.method)
 
-    if(request.method === "GET" && paths[1] === "users" && paths.lenght === 2) {
+    if(request.method === "GET" && paths[1] === "users" && paths.length === 2) {
         response.write(JSON.stringify(Users))
     } 
     else if(request.method === "GET" && paths[1] === "users" && paths[2]) {
@@ -33,6 +33,7 @@ const server = http.createServer(function (request, response) {
             response.write("User Not Found")
         }
     }
+
     else if (request.method === "POST" && paths[1] === "users") {
         let data = ""
 
@@ -49,6 +50,26 @@ const server = http.createServer(function (request, response) {
         response.statusCode = 201
         response.write("User Created")
     }
+
+    else if (request.method === "POST" && paths[1] === "users") {
+        let data = ""
+
+        request.on("data", function (chunks) {
+            data += chunks
+        })
+
+        request.on("end", function () {
+            const obj = JSON.parse(data.toString())
+            Users.push(obj)
+        
+        })
+
+        response.statusCode = 201
+        response.write("User Created")
+    }
+
+    
+
     else {
         response.write("Not Found")
     }
